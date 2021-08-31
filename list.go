48c0c05dfe1ce468c5dffe648497a754832fcdd4
Manul228 <Manul228@users.noreply.github.com>
2021-08-31 14:16:43 +0300

    oddevenlist

--- list.go
+++ list.go
@@ -336,3 +336,22 @@ func RemoveElements(head *ListNode, val int) *ListNode {
 
 	return head
 }
+
+func OddEvenList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	odd, even, evenHead := head, head.Next, head.Next
+
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+
+	odd.Next = evenHead
+
+	return head
+}
